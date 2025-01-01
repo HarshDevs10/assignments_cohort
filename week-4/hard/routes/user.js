3b4651637fb91ea4b53292d3e9ca8204f91bd547
HarshDevs10 <harshdevsinghthakur8@gmail.com>
2025-01-01 18:24:06 +0000

    week-4 hard user/login logic

--- week-4/hard/routes/user.js
+++ week-4/hard/routes/user.js
@@ -4,6 +4,8 @@ const userMiddleware = require("../middleware/user");
 const { mongoose } = require("mongoose");
 const bcrypt = require('bcrypt')
 const { User } = require('../database/index')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 // User Routes
 router.post('/signup', async(req, res) => {
@@ -32,8 +34,54 @@ router.post('/signup', async(req, res) => {
     })
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
      // Implement user login logic
+     const { email, password } = req.body
+
+     let user = null
+     try{
+        user = User.findOne({
+            email
+        })
+        if (!user){
+            return res.json({
+                mes: "you have not signed up. first signup"
+            })
+        }
+     }
+     catch(err){
+        return res.json({
+            mes: "an error occured while finding the user",
+            err: err
+        })
+     }
+
+
+     const passwordCompare = await bcrypt.compare(password, user.password)
+     if (passwordCompare){
+        let token = null
+        try{
+            token = jwt.sign({
+                id: user._id
+            }, process.env.User_Password)
+        }
+        catch(err){
+            return req.json({
+                mes: "an error occured while making the token"
+            })
+        }
+
+        res.json({
+            mes: "you have successfully signed in",
+            token: token
+        })
+     }
+     else{
+        res.json({
+            mes: "your password is wrong"
+        })
+     }
+
 });
 
 router.get('/todos', userMiddleware, (req, res) => {
